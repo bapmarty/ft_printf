4e7101eb71404bb12261d5e1935ae3cd8891371a
baptistemrt <contact@baptistemrt.fr>
2021-02-21 03:12:28 +0100

    :sparkles: Introduce parsing printf

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -11,6 +11,59 @@
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static int	parse_width_length(t_printf *f, int i)
+{
+	int	wl;
+
+	wl = 0;
+	while (f->fmt[i] >= '0' && f->fmt[i] <= '9')
+	{
+		wl = wl * 10 + f->fmt[i] - '0';
+		i++;
+	}
+	if (f->p == 1)
+		f->l = wl;
+	else
+		f->w = wl;
+	return (i);
+}
+
+static int	parse_wildcard(t_printf *f, int i)
+{
+	if (f->p == 1)
+		f->l = va_arg(f->ap, int);
+	else
+		f->w = va_arg(f->ap, int);
+	i++;
+	return (i);
+}
+
+static int	parse_parameters(t_printf *f, int i)
+{
+	if (f->fmt[i] == '-')
+	{
+		f->m = 1;
+		i++;
+	}
+	if (f->fmt[i] >= '0' && f->fmt[i] <= '9')
+		i = parse_width_length(f, i);
+	else if (f->fmt[i] == '*')
+		i = parse_wildcard(f, i);
+	if (f->fmt[i] == '.')
+	{
+		f->p = 1;
+		i++;
+		if (f->fmt[i] >= '0' && f->fmt[i] <= '9')
+			i = parse_width_length(f, i);
+		else if (f->fmt[i] == '*')
+			i = parse_wildcard(f, i);
+	}
+	f->s = f->fmt[i];
+	return (i);
+}
+
+
 static void	parse(t_printf *f)
 {
 	int	i;
@@ -20,6 +73,16 @@ static void	parse(t_printf *f)
 	while (f->fmt[i])
 	{
 		init_params(f);
+		if (f->fmt[i] == '%')
+		{
+			i = parse_parameters(f, i + 1);
+		}
+		else
+		{
+			ft_putchar(f->fmt[i]);
+			f->len++;
+		}
+		i++;
 	}
 }
 
